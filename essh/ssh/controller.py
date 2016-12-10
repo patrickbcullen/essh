6de7cd24c3baa36b3a00121332bd0dc8217338d4
Patrick Cullen <patrick.cullen@washpost.com>
2016-12-10 17:47:55 -0500

    try extra usernames if the no username command fails

--- essh/ssh/controller.py
+++ essh/ssh/controller.py
@@ -1,11 +1,27 @@
 from subprocess import call
+import os
 
 class SSHController:
     def __init__(self, keypair_dir, keypair_name, keypair_extension, host):
         self.keypair = '%s/%s.%s' % (keypair_dir, keypair_name, keypair_extension)
         self.host = host
+        self.alternate_usernames = ['centos', 'ec2-user', 'ubuntu', 'admin', 'bitnami', 'root']
+        self.devnull = open(os.devnull, 'w')
 
     def ssh(self, command=''):
-        ssh_cmd = "ssh -i %s %s%s" % (self.keypair, self.host, command)
-        call(ssh_cmd, shell=True)
+        return_code = self._ssh(command)
+        if return_code == 255:
+            for username in self.alternate_usernames:
+                return_code = self._ssh(command, username)
+                if return_code != 255:
+                    return
 
+
+
+    def _ssh(self, command, username=None):
+        ssh_cmd = "ssh -q -i %s " % self.keypair
+        if username:
+            ssh_cmd += "%s@" % username
+        ssh_cmd += "%s%s" % (self.host, command)
+
+        return call(ssh_cmd, shell=True)
